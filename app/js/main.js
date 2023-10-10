b9690433fd80681f0d12a9a5bc344b838ed01c40
sucser1215 <successcula@gmail.com>
2023-10-10 17:14:41 +0300

    added pruduct

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,13 @@ $(function () {
     autoplaySpeed: 2000,
   });
 
- 
+ $(".star").rateYo({
+   rating: 3.6,
+   starWidth: "17px",
+   normalFill: "#ccccce",
+   ratedFill: "#ffc35b",
+   readOnly: true,
+ });
+
+
 });
